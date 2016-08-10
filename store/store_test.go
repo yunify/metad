848ca3cc375d9278f6a29a266feddff22aab0fe3
jolestar <jolestar@gmail.com>
2016-08-10 15:09:46 +0800

    more store test.

--- store/store_test.go
+++ store/store_test.go
@@ -7,8 +7,55 @@ import (
 	"fmt"
 )
 
-func TestStore(t *testing.T) {
+func TestStoreBasic(t *testing.T) {
 	store := New()
+
+	val, ok := store.Get("/foo")
+	assert.False(t, ok)
+	assert.Nil(t, val)
+
+	store.Set("/foo", false, "bar")
+
+	val, ok = store.Get("/foo")
+	assert.True(t, ok)
+	assert.Equal(t, "bar", val)
+
+	store.Delete("/foo")
+
+	val, ok = store.Get("/foo")
+	assert.False(t, ok)
+	assert.Nil(t, val)
+}
+
+func TestStoreDir(t *testing.T) {
+	store := New()
+
+	store.Set("/foo", true, "")
+
+	store.Set("/foo/foo1", true, "")
+
+	val, ok := store.Get("/foo/foo1")
+	assert.True(t, ok)
+	mapVal, mok := val.(map[string]interface{})
+	assert.True(t, mok)
+	assert.Equal(t, 0, len(mapVal))
+
+	store.Set("/foo/foo1/key1", false, "val1")
+	val, ok = store.Get("/foo/foo1/key1")
+	assert.True(t, ok)
+	assert.Equal(t, "val1", val)
+
+	store.Delete("/foo/foo1")
+
+	val, ok = store.Get("/foo/foo1")
+	assert.False(t, ok)
+	assert.Nil(t, val)
+
+}
+
+func TestStoreBulk(t *testing.T) {
+	store := New()
+
 	//store.Set("/clusters", true, nil)
 	values := make(map[string]string)
 	for i := 1; i <= 10; i++ {
@@ -25,4 +72,5 @@ func TestStore(t *testing.T) {
 	val, ok = store.Get("/clusters/1/ip")
 	assert.True(t, ok)
 	assert.Equal(t, "192.168.0.1", val)
+
 }
