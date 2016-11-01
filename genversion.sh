5a63e735b724dba46f0906eee16c511941d121f6
jolestar <jolestar@gmail.com>
2016-11-01 15:07:17 +0800

    clean build script

--- genversion.sh
+++
@@ -1,9 +0,0 @@
-VERSION=${TRAVIS_TAG:-1.0.dev}
-GIT_VERSION=$(git rev-parse --verify -q HEAD)
-cp version.go version.go.bak
-cat <<EOT > version.go
-package main
-
-const VERSION = "$VERSION"
-const GIT_VERSION = "$GIT_VERSION"
-EOT
